1a417d3215c2125b1d93c6e02af65552ac2907af
user161182 <gabriel.sgaspar@yahoo.com.br>
2020-01-31 20:59:32 -0500

    Adicionando o pacote SwiftfyJSON

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SwifteriOS
+import CoreML
 
 class ViewController: UIViewController {
     
@@ -15,25 +16,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
     
+    let sentimentClassifier = TextClassifier()
+    
     // Iniciando uma instancia da framework. Passando para ela os dados necessarios para autenticar com a API.
     let swifter = Swifter(consumerKey: APIData.API_KEY, consumerSecret: APIData.SECRET_KEY)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Temos dois clojures abaixo, um que e chamado caso a busca seja bem sucedida e outro que e chamado caso aconteca algum erro
-        // count: quantidade de tweets por pagina, o maximo e 100.
-        // extended: queremos tweets completos e nao truncados.
-        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
-            print(results)
-        }) { (error) in
-            print(error)
-        }
+        
+        // Abaixo estamos fazendo a predicao de uma simples frase e acessando a label atribuida pelo modelo.
+        let prediction =  try! sentimentClassifier.prediction(text: "I love banana")
+        print(prediction.label)
         
     }
 
     @IBAction func predictPressed(_ sender: Any) {
-    
-    
+        // Temos dois clojures abaixo, um que e chamado caso a busca seja bem sucedida e outro que e chamado caso aconteca algum erro
+        // count: quantidade de tweets por pagina, o maximo e 100.
+        // extended: queremos tweets completos e nao truncados.
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+            //print(results)
+        }) { (error) in
+            print(error)
+        }
     }
     
 }
