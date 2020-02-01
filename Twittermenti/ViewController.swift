b661739e5783c7bddff35a6f550e99aaad170d4a
user161182 <gabriel.sgaspar@yahoo.com.br>
2020-01-31 20:34:26 -0500

    Buscando tweets na API do twitter

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -15,10 +15,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
     
-    let swifter = Swifter(consumerKey: <#T##String#>, consumerSecret: <#T##String#>)
+    // Iniciando uma instancia da framework. Passando para ela os dados necessarios para autenticar com a API.
+    let swifter = Swifter(consumerKey: APIData.API_KEY, consumerSecret: APIData.SECRET_KEY)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Temos dois clojures abaixo, um que e chamado caso a busca seja bem sucedida e outro que e chamado caso aconteca algum erro
+        // count: quantidade de tweets por pagina, o maximo e 100.
+        // extended: queremos tweets completos e nao truncados.
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+            print(results)
+        }) { (error) in
+            print(error)
+        }
         
     }
 
